c9c03ec4188e8b71d5e22221a2cb34e07e0f6883
diejofre <diego-jofre@live.com.ar>
2021-01-20 17:11:38 -0300

    primeros cambios

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require( 'express' );
+const chalk = require('chalk')
 const morgan = require('morgan'); //middleware application logger
 const nunjucks = require( 'nunjucks' );
 
@@ -10,6 +11,8 @@ app.engine('html', nunjucks.render); // cuando le den archivos html a res.render
 nunjucks.configure('views'); // apunta a nunjucks al directorio correcto para los templates
 
 app.use(morgan('tiny'))
+app.use(express.static('./public'))
+
 
 let tweetsDeEjemplo = [
     { id: 1, name: "juan", content: "este es un tweeettt de juan" },
@@ -21,8 +24,28 @@ app.get('/', function (req, res) {
     res.render( 'index', { tweets: tweetsDeEjemplo });
 });
 
+
+app.use(function (req, res, next) {
+    //console.log(req)
+    next()
+    // hacé tu logueo acá, probá console.log(req)
+    // llamá a `next()`. Sino tu app recibirá pedidos 
+    // pero no responderá adecuadamente.
+})
+
+app.use(function(req, res, next){
+    const data = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+    res.render( 'index', {title: 'Hall of Fame', personas: data} );
+});
+
+app.use('/especial', function (req, res, next) {
+    console.log('llegaste a un área especial')
+    next()
+})
+
 app.listen(3000, function(){
     console.log('Estas escuhando en el puerto 3000')
+
 });
 
 
